c06b7a6b43acf88e9c3d7ab97acfc522e27577fd
“Denis <deni.kaiju@gmail.com>
2024-04-28 13:04:40 +0200

    Changed as on lesson 15. Django Models

--- src/users/models.py
+++ src/users/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-
 class User(models.Model):
-    email = models.CharField(max_length=30)
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=100)
     first_name = models.CharField(max_length=30)
     second_name = models.CharField(max_length=30)
-    password = models.CharField(max_length=100)
-    role = models.CharField(max_length=20)
+    avatar = models.CharField(max_length=255)  # Предполагается, что ссылка на аватар будет длинной строкой
+    level = models.IntegerField(default=1)  # По умолчанию уровень пользователя - 1
