fec4abe33edc5f2e4a8229ff7dd559c364111a93
“Denis <deni.kaiju@gmail.com>
2024-05-01 19:19:47 +0200

    Workflows update

--- src/users/models.py
+++ src/users/models.py
@@ -1,15 +1,17 @@
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, Group, Permission # noqa
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    Group,
+    Permission,
+)
 from django.utils import timezone
 from django.db import models
 
 from .managers import UserManager
 
+
 class User(AbstractBaseUser, PermissionsMixin):
-    email = models.CharField(
-        max_length=30, 
-        unique=True,
-        default="example@example.com"
-        )
+    email = models.CharField(max_length=30, unique=True, default="example@example.com") # noqa
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     is_staff = models.BooleanField(default=False)
@@ -27,18 +29,20 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     # Добавляем related_name для поля groups и исправляем обратные запросы
     groups = models.ManyToManyField(Group, related_name="users", blank=True)
-    user_permissions = models.ManyToManyField(Permission, related_name="users", blank=True)
+    user_permissions = models.ManyToManyField(
+        Permission, related_name="users", blank=True
+    )
 
     def get_full_name(self):
         """
         Return the first_name plus the last_name, with a space in between.
         """
         return f"{self.first_name} {self.last_name}".strip()
-    
+
     def get_short_name(self):
         """Return the short name for the user."""
         return self.first_name
-    
+
     def __str__(self):
         if self.first_name and self.last_name:
             return self.get_full_name()
